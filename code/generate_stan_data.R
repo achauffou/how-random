ea7cb6065f5669fe4bacad154facb713772499d0
achauffou <alain.chauffoureaux@gmail.com>
2021-05-30 12:43:25 +0200

    Prepare model pol_logit_d01

--- code/generate_stan_data.R
+++ code/generate_stan_data.R
@@ -229,3 +229,77 @@ generate_stan_start_values.pol_logit_c <- function(nb_pla, nb_pol, nb_sites) {
     sigma_l = 0.1
   )
 }
+
+#' Simple model to generate plant-pollinator interaction data (IDs in array)
+#' 
+generate_stan_data.pol_logit_d <- function(nb_pla, nb_pol, nb_sites) {
+  # Generate degree and optimal suitability:
+  K_pla <- rbetacut(nb_pla, 3, 2, high_cut = 0.9)
+  K_pol <- rbetacut(nb_pol, 3, 2, high_cut = 0.9)
+  S_opt_pla <- rnorm(nb_pla, 0, 1)
+  S_opt_pol <- rnorm(nb_pol, 0, 1)
+  env_sit <- rnorm(nb_sites, 0, 1)
+  S_pla <- abs(outer(S_opt_pla, env_sit, "-"))
+  S_pol <- abs(outer(S_opt_pol, env_sit, "-"))
+  
+  # Create data.table with all interactions:
+  data <- expand.grid(
+    pla_id = 1:nb_pla, pol_id = 1:nb_pol, site_id = 1:nb_sites) %>%
+    data.table::as.data.table()
+  data[, ':='(
+    D = K_pla[pla_id] * K_pol[pol_id],
+    S = S_pla[cbind(pla_id, site_id)] * S_pol[cbind(pol_id, site_id)]
+  )]
+  
+  # Sample parameters:
+  alpha <- rnorm(nb_sites, 0, 1)
+  beta_pla <- rnorm(nb_pla, 0, 1)
+  beta_pol <- rnorm(nb_pol, 0, 1)
+  lambda <- rnorm(nb_sites, 0, 1)
+  nu <- rnorm(nb_sites, 0, 1)
+  
+  # Compute response variable:
+  data[, p := boot::inv.logit(
+    alpha[site_id] + beta_pla[pla_id] + beta_pol[pol_id] + lambda[site_id] * D + 
+      nu[site_id] * S
+  )]
+  data[, Y := purrr::map_int(p, ~rbinom(1, 1, .))]
+  
+  # Return data specified as a list:
+  list(
+    nb_pla = nb_pla,
+    nb_pol = nb_pol,
+    nb_sites = nb_sites,
+    nb_int = nrow(data),
+    Y_array = data[, .(Y, site_id, pla_id, pol_id)],
+    D_pla = K_pla,
+    D_pol = K_pol,
+    S_pla = S_pla,
+    S_pol = S_pol,
+    alpha = alpha,
+    beta_pol = beta_pol,
+    beta_pla = beta_pla,
+    lambda = lambda,
+    nu = nu
+  )
+}
+
+generate_stan_start_values.pol_logit_d <- function(nb_pla, nb_pol, nb_sites) {
+  list(
+    zalpha = rep(0, nb_sites),
+    zbeta_pol = rep(0, nb_pol),
+    zbeta_pla = rep(0, nb_pla),
+    znu = rep(0, nb_sites),
+    zlambda = rep(0, nb_sites),
+    alpha_bar = 0,
+    beta_pol_bar = 0,
+    beta_pla_bar = 0,
+    nu_bar = 0,
+    lambda_bar = 0,
+    sigma_a = 0.1,
+    sigma_bp = 0.1,
+    sigma_bpl = 0.1,
+    sigma_n = 0.1,
+    sigma_l = 0.1
+  )
+}
