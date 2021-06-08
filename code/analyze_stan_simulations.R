019150b52a9a46b314071218b788b5fc22463929
achauffou <alain.chauffoureaux@gmail.com>
2021-06-08 12:23:40 +0200

    Make function to plot parameter errors

--- code/analyze_stan_simulations.R
+++ code/analyze_stan_simulations.R
@@ -20,3 +20,44 @@ analyse_stan_sim <- function(spec, data, start, fits) {
   # Return RStan fit summary:
   list(rstan::summary(rstan_fit)$summary)
 }
+
+
+# Miscellaneous functions for the Stan simulations analyses ====================
+#' Plot the errors of a parameter ordered by its number of data (mean + 95% CI)
+#' 
+misc_stan_analyses_plot_param_error <- function(
+  fit, param, exact_values, nb_data, aggregate_by_nb_data = TRUE
+) {
+  # Create the data.table for the plot:
+  plt_data <- as.data.frame(fit, pars = param) %>%
+    data.table::as.data.table() %>%
+    data.table::melt(id.vars = character())
+  
+  # Add variable ID, error and number of data:
+  if (length(exact_values) > 1) {
+    plt_data[, id := as.integer(stringr::str_extract(variable, "[0-9]+"))]
+  } else {
+    plt_data[, id := 1]
+  }
+  plt_data[, ':='(
+    error = value - exact_values[id],
+    nb_data = nb_data[id]
+  )]
+  
+  # Make the boxplot:
+  plt <- ggplot(plt_data, aes_string(
+    x = "nb_data", y = "error", 
+    group = ifelse(aggregate_by_nb_data, "nb_data", "id"), width = 0.4
+  )) +
+    stat_summary(geom = "point", fun = mean, position=position_dodge(.9)) +
+    stat_summary(geom = "errorbar", fun.min = function(x) quantile(x, 0.05),
+                 fun.max = function(x) quantile(x, 0.95), position=position_dodge(.9)) +
+    xlab("Amount of data") +
+    ylab("Error") +
+    theme_classic()
+  if (length(unique(nb_data)) == 1) {
+    plt <- plt + theme(axis.title.x = element_blank(), axis.text.x = element_blank(),
+                axis.ticks.x = element_blank(), axis.line.x = element_blank())
+  }
+  plt
+}
