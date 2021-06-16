4c1303105c9a5d0d29359f11b13318827a7ee556
achauffou <alain.chauffoureaux@gmail.com>
2021-06-16 11:04:59 +0200

    Make posterior error plots (again)

--- code/analyze_stan_simulations.R
+++ code/analyze_stan_simulations.R
@@ -41,11 +41,66 @@ stan_sim_analyses_plot_param_post_true <- function(
 stan_sim_analyses_plot_save_params_post_true <- function(
   fit, data, params, res_folder
 ) {
-  lapply(params, function(x) {suppressMessages({
+  lapply(params, function(x) {suppressWarnings({suppressMessages({
     stan_sim_analyses_plot_param_post_true(fit, data, x) %>%
       ggsave(file.path(res_folder, paste0("param_post_true_ ", x, ".pdf")), ., 
              device = "pdf")
-  })})
+  })})})
+}
+
+#' Plot the errors of a parameter ordered by its number of data (mean + 95% CI)
+#' 
+stan_sim_analyses_plot_param_error <- function(
+  fit, param, exact_values, nb_data, aggregate_by_nb_data = TRUE
+) {
+  # Create the data.table for the plot:
+  plt_data <- as.data.frame(fit, pars = param) %>%
+    data.table::as.data.table() %>%
+    data.table::melt(id.vars = character())
+  
+  # Add variable ID, error and number of data:
+  if (length(exact_values) > 1) {
+    plt_data[, id := as.integer(stringr::str_extract(variable, "[0-9]+"))]
+  } else {
+    plt_data[, id := 1]
+  }
+  plt_data[, ':='(
+    error = value - exact_values[id],
+    nb_data = nb_data[id]
+  )]
+  
+  # Make the boxplot:
+  plt <- ggplot(plt_data, aes_string(
+    x = "nb_data", y = "error", 
+    group = ifelse(aggregate_by_nb_data, "nb_data", "id"), width = 0.4
+  )) +
+    geom_hline(yintercept = 0.0, color = "grey") +
+    stat_summary(geom = "point", fun = mean, position=position_dodge(.9)) +
+    stat_summary(geom = "errorbar", fun.min = function(x) quantile(x, 0.05),
+                 fun.max = function(x) quantile(x, 0.95), position=position_dodge(.9)) +
+    xlab("Amount of data") +
+    ylab("Error") +
+    ylim(-3.0, 3.0) +
+    theme_classic()
+  if (length(unique(nb_data)) == 1) {
+    plt <- plt + theme(axis.title.x = element_blank(), axis.text.x = element_blank(),
+                       axis.ticks.x = element_blank(), axis.line.x = element_blank())
+  }
+  plt
+}
+
+#' Plot the errors of several parameters ordered by their number of data
+#' 
+stan_sim_analyses_plot_save_params_errors <- function(
+  fit, data, params, id_names, res_folder
+) {
+  lapply(1:length(params), function(x) {suppressWarnings({suppressMessages({
+    stan_sim_analyses_plot_param_error(
+      fit, params[x], data[[params[x]]], 
+      data$Y_array[, .N, by = c(id_names[x])][order(get(id_names[x]))][['N']]) %>%
+      ggsave(file.path(res_folder, paste0("param_error_ ", params[x], ".pdf")), ., 
+             device = "pdf")
+  })})})
 }
 
 
@@ -59,6 +114,12 @@ analyse_stan_sim.pol_binom_01 <- function(
   c("beta", "gamma_pla", "gamma_pol", "sigma_beta", "sigma_gamma_pla",
     "sigma_gamma_pol") %>%
     stan_sim_analyses_plot_save_params_post_true(rstan_fit, data, ., res_folder)
+  
+  # Plot posterior error of multilevel parameters:
+  stan_sim_analyses_plot_save_params_errors(
+    rstan_fit, data, c("beta", "gamma_pla", "gamma_pol"), 
+    c("site_id", "pla_id", "pol_id"), res_folder
+  )
 }
 
 #' Analyse pollination binomial with intercepts only
@@ -70,6 +131,12 @@ analyse_stan_sim.pol_binom_02 <- function(
   c("alpha", "beta", "gamma_pla", "gamma_pol", "sigma_beta", "sigma_gamma_pla",
     "sigma_gamma_pol") %>%
     stan_sim_analyses_plot_save_params_post_true(rstan_fit, data, ., res_folder)
+  
+  # Plot posterior error of multilevel parameters:
+  stan_sim_analyses_plot_save_params_errors(
+    rstan_fit, data, c("beta", "gamma_pla", "gamma_pol"), 
+    c("site_id", "pla_id", "pol_id"), res_folder
+  )
 }
 
 #' Analyse pollination binomial with intercepts and slopes
@@ -82,4 +149,10 @@ analyse_stan_sim.pol_binom_03 <- function(
     "lambda", "nu", "sigma_beta", "sigma_gamma_pla", "sigma_gamma_pol",
     "sigma_lambda", "sigma_nu") %>%
     stan_sim_analyses_plot_save_params_post_true(rstan_fit, data, ., res_folder)
+  
+  # Plot posterior error of multilevel parameters:
+  stan_sim_analyses_plot_save_params_errors(
+    rstan_fit, data, c("beta", "gamma_pla", "gamma_pol", "lambda", "nu"), 
+    c("site_id", "pla_id", "pol_id", "site_id", "site_id"), res_folder
+  )
 }
